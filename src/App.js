34487fb2647c611a2724a59666bcee0273be724e
Zokirkhon <78657264+Zokirkhon1002@users.noreply.github.com>
2021-08-24 01:49:08 +0900

    n
    m

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Output from './components/Output';
+
+function App() {
+  return (
+    <div className="App">
+      <Output />
+    </div>
+  );
+}
+
+export default App;
